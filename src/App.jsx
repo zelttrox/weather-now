512a5ab4f89eb04f267a22423e5e022f9d0b47a9
enzo <enzo.granana@ynov.com>
2025-05-05 14:22:34 +0200

    Test useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import service from "../api/service"
+import { useState, useEffect } from "react";
 
 function App() {
 
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log("oeziud");
+            setTime(prevTime => prevTime + 10);
+        }, 1000);
+
+        return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, []);
+
     return (
         <>
-        <h1> Welcome to weather now </h1>
-        <p> {service.GetWeather(43.610767, 3.876716)} </p>
+            <h1> Welcome to weather now </h1>
+            <p> {time} </p>
         </>
     )
 }
