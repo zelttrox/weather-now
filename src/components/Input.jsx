67d00a76f85c979c2a5a31cf4277f228b68d3424
enzo <enzo.granana@ynov.com>
2025-05-13 13:03:13 +0200

    Add input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+import "./Input.css"
+
+function Input() {
+  return (
+    <div class="cont">
+        <input type="text" placeholder="City" class="city-input"/>
+        <button class="submit-btn"> Get Weather </button>
+    </div>
+  )
+}
+
+export default Input
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom/client'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Display from './components/Display'
+import Input from './components/Input'
 
 // Create root element
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -16,6 +17,7 @@ root.render(
 
   <Header/>
   <Display/>
+  <Input/>
   <Footer/>
 
   </React.StrictMode>
