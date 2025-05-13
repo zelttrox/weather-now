1495b5e1c08661757123f50a03dbb72b5f8fbd69
enzo <enzo.granana@ynov.com>
2025-05-13 12:51:25 +0200

    Fix data display + remove data.js

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,17 +1,25 @@
 import "./Display.css"
 
 function Display() {
+
+  var data = {
+    temperature: 14,
+    description: "Sunny",
+    humidity: 80,
+    windSpeed: 8.5
+  }
+
   return (
     <div class="cont">
       <img class="image" src="./images/sunny.png"/>
       <div class="cont-temp">
-        <p class="temp-text"> 0°C </p>
+        <p class="temp-text"> {`${data.temperature}°C`} </p>
       </div>
-      <p class="desc-text"> Sunny </p>
+      <p class="desc-text"> {data.description} </p>
       <div class="cont-infos">
         <div class="cont-info">
         <i class="fa-solid fa-droplet"></i>
-          <p class="info-text"> 0% </p>
+          <p class="info-text"> {`${data.humidity}%`} </p>
         </div>
         <div class="cont-info">
         <i class="fa-solid fa-wind"></i>
